9d53583007bd06fcf78bffc532c87a3cda095aa7
Avengerandy007 <Avengerandy007@users.noreply.github.com>
2025-07-28 16:06:47 +0300

    Moved window class method definitions to separate cpp file

---
+++ src/window.cpp
@@ -0,0 +1,35 @@
+#include "window.hpp"
+
+void Window::Render(){
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+}
+
+int Window::Setup(){
+	if (SDL_Init(SDL_INIT_VIDEO) < 0){
+		printf("Could not init SDL video!");
+		return -1;
+	}
+
+	window = SDL_CreateWindow("Shitty breakout", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 650, 450, SDL_WINDOW_SHOWN);
+	
+	if (window == NULL){
+		printf("Could not create SDL window!");
+		return -1;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+
+	if (renderer == NULL){
+		printf("Could not create SDL renderer!");
+		return -1;
+	}
+	
+	return 0;
+}
+
+Window::~Window(){
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
--- src/window.hpp
+++ src/window.hpp
@@ -10,37 +10,7 @@ class Window{
 public:
 	SDL_Renderer* renderer;
 
-	void Render(){
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-	}
-
-	int Setup(){
-		if (SDL_Init(SDL_INIT_VIDEO) < 0){
-			printf("Could not init SDL video!");
-			return -1;
-		}
-
-		window = SDL_CreateWindow("Shitty breakout", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 650, 450, SDL_WINDOW_SHOWN);
-		
-		if (window == NULL){
-			printf("Could not create SDL window!");
-			return -1;
-		}
-
-		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-
-		if (renderer == NULL){
-			printf("Could not create SDL renderer!");
-			return -1;
-		}
-		
-		return 0;
-	}
-
-	~Window(){
-		SDL_DestroyWindow(window);
-		SDL_DestroyRenderer(renderer);
-		SDL_Quit();
-	}
+	void Render();
+	int Setup();
+	~Window();
 };
